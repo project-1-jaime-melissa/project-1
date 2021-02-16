4e928ae3afd3dd4b1ca02bf64089355c2bd408ec
jaimerobbins <robbins_jaime@hotmail.com>
2021-02-16 16:10:11 -0500

    added image carrosel functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 const app = {};
 
+//global variables
+app.imageCounter = 0;
+app.imageCounter2 = 0;
+app.imageArray = [
+    '<img src="./assets/gallery-image-1.jpg" alt="Small yacht in the horizon below slightly cloudy sky.">',
+    '<img src="./assets/gallery-image-2.jpg" alt="Right side of a yacht from perspecitve of inside the boat looking out over the ocean.">',
+    '<img src="./assets/gallery-image-3.jpg" alt="Back left side of a yacht from perspective of inside the boat looking out to the ocean">'
+];
+
 //initialize
 app.init = () => {
     //add event listeners for modals
@@ -17,9 +26,24 @@ app.init = () => {
             modal.style.display = 'none';
         });
     }
+    
+    //add initial gallery images
+    app.pageImage();
+
+    //add event listeners for image gallery
+    const rightArrow = document.querySelector('.fa-chevron-circle-right');
+    const leftArrow = document.querySelector('.fa-chevron-circle-left');
+    
+    rightArrow.addEventListener('click', () => {
+        app.pageImage('right');
+    });
+    
+    leftArrow.addEventListener('click', () => {
+        app.pageImage('left');
+    });
 };
 
-//function to create event listeners
+//function to create modal event listeners
 app.addListener = (className) => {
     const container = document.querySelector(className + ' .button');
     container.addEventListener('click', () => {
@@ -32,6 +56,43 @@ app.showModal = (containerClass) => {
     const modal = document.querySelector(containerClass + ' .modal')
     modal.style.display = 'unset'; 
 };
+
+//function to page through images
+app.pageImage = (direction) => {
+
+    //increment/decrement counter
+    if (direction === 'right') {
+        app.imageCounter++
+    } else if (direction === 'left') {
+        app.imageCounter--
+    } else {
+        app.imageCounter = 0;
+    }
+    console.log('first', app.imageCounter, app.imageCounter2)
+
+    //reset counter if at end
+    if (app.imageCounter === app.imageArray.length) {
+        console.log('equal to length')
+        app.imageCounter = 0;
+    } else if (app.imageCounter < 0) {
+        console.log('less than 0')
+        app.imageCounter = app.imageArray.length - 1;
+    }
+
+    //set counter for 2nd image
+    app.imageCounter2 = app.imageCounter + 1;
+    if (app.imageCounter2 === app.imageArray.length) {
+        app.imageCounter2 = 0;
+    }
+
+    //set image items to display
+    const imageItem1 = app.imageArray[app.imageCounter];
+    const imageItem2 = app.imageArray[app.imageCounter2];
+    const imageUL = document.querySelector('ul.image-container');
+
+    imageUL.querySelector('li:nth-of-type(1n)').innerHTML = imageItem1;
+    imageUL.querySelector('li:nth-of-type(2n)').innerHTML = imageItem2;
+}
 
 //DOM ready
 document.addEventListener('DOMContentLoaded', (event) => {
